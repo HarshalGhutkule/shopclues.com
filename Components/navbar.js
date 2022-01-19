421254a5df7968de36faeed6fd204493cd1c323b
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2022-01-19 15:22:12 +0530

    Sign-in & Register page done

--- Components/navbar.js
+++ Components/navbar.js
@@ -1,10 +1,20 @@
 
+
+// dorpdown options mouseover function
+
 function mouseoverNavigation(ele){
     let nav1 = document.querySelector(`#${ele}>div`);
     nav1.style.display = "block";
+    document.querySelector(`#${ele}>a`).style.backgroundColor = "white";
+    document.querySelector(`#${ele}>a`).style.color = "#24a3b5";
 }
+
+
+// dorpdown options mouseout function
 
 function mouseoutNavigation(ele){
     let nav1 = document.querySelector(`#${ele}>div`);
     nav1.style.display = "none";
+    document.querySelector(`#${ele}>a`).style.backgroundColor = "#24a3b5";
+    document.querySelector(`#${ele}>a`).style.color = "white";
 }
