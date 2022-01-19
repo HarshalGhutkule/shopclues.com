cececc23762a6eef3fcbefb47a106a20725dc72f
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2022-01-19 18:32:35 +0530

    Sign-in & Register page done

--- Components/navbar.js
+++ Components/navbar.js
@@ -18,3 +18,172 @@ function mouseoutNavigation(ele){
     document.querySelector(`#${ele}>a`).style.backgroundColor = "#24a3b5";
     document.querySelector(`#${ele}>a`).style.color = "white";
 }
+
+// function to openSigninPage
+let openSigninPage = ()=> {
+    document.getElementById("back").style.display = "block";
+}
+
+// function for signOut
+
+let signOut = () => {
+        document.getElementById("login_sucess").textContent = "Sign In";
+        document.getElementById("login_sucess").style.fontSize = "16px";
+        document.getElementById("login_sucess1").textContent = "Login/Register";
+        document.getElementById("signout").style.display = "none";
+}
+
+// functions for sign-in and regiser page
+
+// function to show login,register div
+
+let div1 = document.querySelector(".signinShow");
+let div2 = document.querySelector(".signup");
+let h2_1 = document.querySelector("#select_op>h2:nth-child(1)");
+let h2_2 = document.querySelector("#select_op>h2:nth-child(2)");
+
+let showLogin = () => {
+    div2.style.display = "none";
+    h2_2.style.color = "black";
+    h2_2.style.borderBottom = "none";
+
+    div1.style.display = "block";
+    h2_1.style.color = "#55b8c6";
+    h2_1.style.borderBottom = "2px solid #55b8c6";
+
+}
+
+let showRegister = () => {
+    div1.style.display = "none";
+    h2_1.style.color = "black";
+    h2_1.style.borderBottom = "none";
+
+    div2.style.display = "block";
+    h2_2.style.color = "#55b8c6";
+    h2_2.style.borderBottom = "2px solid #55b8c6";
+
+}
+
+
+// function for storing userdetail on masai server(register page)
+
+let storedetail = document.getElementById("storedetail");
+
+storedetail.addEventListener("click", postData);
+
+async function postData(event){
+
+    event.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let number = document.getElementById("number").value;
+    let username = document.getElementById("username1").value;
+    let password = document.getElementById("password1").value;
+
+    if(email == "" || number == "" || username == "" || password == ""){
+        alert("Please fill all info");
+    }
+    else{
+        let userdetail = {
+            email,
+            number,
+            username,
+            password
+        }
+
+        userdetail = JSON.stringify(userdetail);
+
+        let url = "https://masai-api-mocker.herokuapp.com/auth/register";
+
+        try{
+            let responce = await fetch(url, {
+                method: "POST",
+                body: userdetail,
+                headers: {
+                    "Content-Type" : "application/json"
+                }
+            })
+
+            let res = await responce.json();
+            console.log(res);
+            if(res.error === true){
+                alert(res.message);
+            }
+            else{
+                alert(res.message);
+            }
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    
+}
+
+// function for checking userdetail on masai server (login page)
+
+let checkdetail = document.getElementById("checkdetail");
+
+checkdetail.addEventListener("click", checkData);
+
+async function checkData(event){
+    event.preventDefault();
+
+    let userData = {
+        username : document.getElementById("username2").value,
+        password : document.getElementById("password2").value, 
+    };
+
+    userData = JSON.stringify(userData);
+
+    try{
+        let url = "https://masai-api-mocker.herokuapp.com/auth/login";
+        let responce = await fetch(url,{
+            method:"POST",
+            body:userData,
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        })
+
+        let res = await responce.json();
+        alert("Login successfully");
+        let user = document.getElementById("username2").value;
+        getUser(user,res.token);
+        document.getElementById("back").style.display = "none";
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+let getUser = async(user,token) => {
+    let url = `https://masai-api-mocker.herokuapp.com/user/${user}`;
+
+    try{
+        let responce = await fetch(url, {
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization" : `Bearer ${token}`
+            }
+        })
+
+        let data = await responce.json();
+        document.getElementById("login_sucess").textContent = data.username;
+        document.getElementById("login_sucess").style.fontSize = "12px";
+        document.getElementById("login_sucess1").textContent = `Welcome ${data.username}`;
+        document.getElementById("signout").style.display = "block";
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+// function to close log and register tab
+
+let close = document.getElementById("closetab");
+
+close.addEventListener("click", ()=>{
+    document.getElementById("back").style.display = "none";
+})
+
