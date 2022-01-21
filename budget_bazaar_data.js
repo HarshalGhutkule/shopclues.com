aa933a0471ae7708325d904c546af11cf240df77
Bhavesh-XLV <95977977+Bhavesh-XLV@users.noreply.github.com>
2022-01-21 16:30:16 +0530

    Add files via upload

---
+++ budget_bazaar_data.js
@@ -0,0 +1,17 @@
+var budget_bazaar = [
+  {
+    img: "https://cdn.shopclues.com/images/banners/PricePont_akb_1Oct21_W_01.jpg",
+  },
+  {
+    img: "https://cdn.shopclues.com/images/banners/PricePont_akb_1Oct21_W_02.jpg",
+  },
+  {
+    img: "https://cdn.shopclues.com/images/banners/PricePont_akb_1Oct21_W_03.jpg",
+  },
+  {
+    img: "https://cdn.shopclues.com/images/banners/PricePont_akb_1Oct21_W_04.jpg",
+  },
+  {
+    img: "https://cdn.shopclues.com/images/banners/PricePont_akb_1Oct21_W_05.jpg",
+  },
+];
---
+++ gadget_store_data.js
@@ -0,0 +1,108 @@
+var gadget_store_data = [
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/99370/200/200/145508146-99370521-1634810951.jpg",
+    name: "i7S TWS Twins Wireless In t...",
+    p1: "379",
+    p2: "2999",
+    p3: "342",
+    dis: "88% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/95357/200/200/142826933-95357827-1606398228.jpg",
+    name: "HBS-730 In the Ear Bluetoot...",
+    p1: "329",
+    p2: "1099",
+    p3: "294",
+    dis: "73% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/81476/200/200/135627858-81476269-1619845151.jpg",
+    name: "MTR MT310 Dual Sim Featu...",
+    p1: "749",
+    p2: "999",
+    p3: "693",
+    dis: "30% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/112883/200/200/151860087-112883707-1625898295.jpg",
+    name: "Innotek Mini K1 Universal W...",
+    p1: "149",
+    p2: "599",
+    p3: "123",
+    dis: "79% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/98012/200/200/144475154-98012302-1584693159.jpg",
+    name: "Refurbished Nokia 1100 Bla...",
+    p1: "1199",
+    p2: "1999",
+    p3: "1045",
+    dis: "47% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/98012/200/200/144475154-98012302-1584693159.jpg",
+    name: "Refurbished Nokia 1100 Bla...",
+    p1: "1199",
+    p2: "1999",
+    p3: "1045",
+    dis: "47% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/112883/200/200/151860087-112883707-1625898295.jpg",
+    name: "Innotek Mini K1 Universal W...",
+    p1: "149",
+    p2: "599",
+    p3: "123",
+    dis: "79% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/98012/200/200/144475154-98012302-1584693159.jpg",
+    name: "Refurbished Nokia 1100 Bla...",
+    p1: "1199",
+    p2: "1999",
+    p3: "1045",
+    dis: "47% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/98012/200/200/144475154-98012302-1584693159.jpg",
+    name: "Refurbished Nokia 1100 Bla...",
+    p1: "1199",
+    p2: "1999",
+    p3: "1045",
+    dis: "47% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/105068/200/200/149071836-105068393-1582034843.jpg",
+    name: "Raptech Compatible Micro...",
+    p1: "189",
+    p2: "999",
+    p3: "161",
+    dis: "83% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images1/thumbnails/102529/200/200/147616538-102529620-1584606292.jpg",
+    name: "3 meter long Universal mobi...",
+    p1: "179",
+    p2: "799",
+    p3: "158",
+    dis: "80% off",
+  },
+  {
+    img: "https://cdn.shopclues.com/images/banners/view_all_demo.jpg",
+    name: "View All",
+  },
+];
+
+// gadget_store_data.forEach((data) => {
+//   let div = document.createElement("div");
+
+//   let img = document.createElement("img");
+//   img.src = data.img;
+
+//   let name = document.createElement("p");
+//   name.innerText = data.name;
+
+//   div.append(img, name);
+
+//   document.querySelector(".yout_gagdet_store").append(img, name);
+// });
