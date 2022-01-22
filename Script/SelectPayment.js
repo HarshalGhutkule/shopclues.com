721fd3e9e5f270e33af89b2516bd9a35b5d6bf84
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2022-01-22 22:55:13 +0530

    Payment selection page done

---
+++ Script/SelectPayment.js
@@ -0,0 +1,36 @@
+ function paymentDone(e) {
+    e.preventDefault();
+    let card_num = document.querySelector(".card_num").value;
+    let MM = document.querySelector("#month").value;
+    let YY = document.querySelector("#year").value;
+    let CVV = document.querySelector("#cvv").value;
+    let card_name = document.querySelector(".card_name").value;
+    console.log("here");
+    if (
+      card_num == 123412341234 &&
+      MM == 04 &&
+      YY == 2024 &&
+      CVV == 123 &&
+      card_name == "Vipin awadhiya"
+    ) {
+      alert("Please wait payment is processing")
+      setTimeout(timeout,2000);
+
+      function timeout(){
+        window.alert("Payment successfull");
+        window.location.href = "index.html";
+      }
+
+      
+    } else {
+      document.querySelector(".invalid").style.display = "block";
+    }
+  }
+
+
+  // show payment amount
+
+  let amountTopay = JSON.parse(localStorage.getItem("paymentAmount"));
+
+  document.getElementById("pay").value = `Pay ${amountTopay}`;
+  
